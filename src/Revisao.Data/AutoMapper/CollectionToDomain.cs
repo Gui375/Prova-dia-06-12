9b6ba8022eeab3c6e3a994ae773f91724459c078
044748 <044748@UNI-LAB2-42>
2023-12-06 20:24:16 -0300

    Add project files.

---
+++ src/Revisao.Data/AutoMapper/CollectionToDomain.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao.Data.AutoMapper
+{
+    public class CollectionToDomain : Profile
+    {
+        public CollectionToDomain()
+        {
+
+            CreateMap<ProdutoCollection, Produto>()
+               .ConstructUsing(q => new Produto(q.CodigoId, q.Nome, q.Descricao, q.Ativo, q.Valor, q.DataCadastro, q.Imagem, q.QuantidadeEstoque, q.EstoqueMinimo));
+
+            CreateMap<CategoriaCollection, Categoria>()
+               .ConstructUsing(q => new Categoria(q.CodigoId, q.Descricao, q.Ativo));
+
+            CreateMap<FornecedorCollection, Fornecedor>()
+                    .ConstructUsing(f => new Fornecedor(f.CodigoId, f.Nome, f.RazaoSocial, f.Cnpj, f.DataCadastro, f.Ativo));
+        }
+    }
+}
---
+++ src/Revisao.Data/Providers/MongoDb/Collections/JogoCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao.Data.Providers.MongoDb.Collections
+{
+    internal class JogoCollection
+    {
+    }
+}
